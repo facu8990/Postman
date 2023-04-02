e1a9ec6a539d93afa7df1bf4ac6795519faf4b55
facu8990 <facu.vdp@gmail.com>
2023-04-02 00:59:16 -0300

    remove keys

--- run-collection.bat
+++ run-collection.bat
@@ -1,5 +1,5 @@
 @echo off
-set apikey="PMAK-640ff23495d74f6ee3a1b7f0-876f1c2f1ecefcc27269455052be1c4d04"
+set apikey="PMAK-xxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 set /p collection="Enter the Collection's ID: "
 set /p environment="Enter the Environment's ID: "
 call npx newman run https://api.getpostman.com/collections/%collection%?apikey=%apikey% -e https://api.getpostman.com/environments/%environment%?apikey=%apikey% -r cli,htmlextra
